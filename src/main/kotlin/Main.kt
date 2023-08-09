877ecc3303195ded9a9016d4608219dd6f0612b1
KIBU._.NJAA <kibunjafranziska@gmail.com>
2023-08-09 13:16:39 +0300

    printing my name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main(args: Array <String>) {
+    println("Hello Aliens!!!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var nishan = Alien()
+
+    nishan.name = "Kibunja"
+
+    println("My name is : ${nishan.name}")
+
 }
